0940609ed6b1743a2d488c1db3a5edae302f8d84
owsy123 <owais.sohail3@gmail.com>
2020-06-04 19:57:00 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>first React App</h1>
+        <h1>first React App from owsy</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
