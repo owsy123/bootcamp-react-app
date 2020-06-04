ae556bfe03f9ecc1075a03a39dbdd1a128fd7e17
owsy123 <owais.sohail3@gmail.com>
2020-06-04 23:37:22 +0500

    change display name owsy to danial

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>first React App from owsy</h1>
+        <h1>Hello Danial </h1>
         <a
           className="App-link"
           href="https://reactjs.org"
